7748c6a46976c4c56af1b4cbfac8746b6dd2ef24
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-20 21:40:17 +0530

    Initial Commit

---
+++ src/main/java/com/capstone/wfh/WfhApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.wfh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/wfh/WfhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.wfh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
