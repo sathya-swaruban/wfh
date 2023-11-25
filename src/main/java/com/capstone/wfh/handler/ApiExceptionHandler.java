076756817021e6392d7ed0ccfb53f22aab444e9e
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-25 21:31:55 +0530

    Update ApiExceptionHandler

---
+++ src/main/java/com/capstone/wfh/handler/ApiExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.capstone.wfh.handler;
+
+import com.capstone.wfh.exception.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@SuppressWarnings(value = "all")
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(DifferentDatesException.class)
+    protected ResponseEntity<String> handleDifferentDatesException(DifferentDatesException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidTimeRangeException.class)
+    protected ResponseEntity<String> handleInvalidTimeRangeException(InvalidTimeRangeException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidToTimeHourException.class)
+    protected ResponseEntity<String> handleInvalidToTimeHourException(InvalidToTimeHourException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidToTimeMinuteException.class)
+    protected ResponseEntity<String> handleInvalidToTimeMinuteException(InvalidToTimeMinuteException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(RequestExistsException.class)
+    protected ResponseEntity<String> handleRequestExistsException(RequestExistsException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(WeekendException.class)
+    protected ResponseEntity<String> handleWeekendException(WeekendException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+}
