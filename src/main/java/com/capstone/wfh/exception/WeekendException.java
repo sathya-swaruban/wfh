1193118cbdad6bb19eac48cb73c50e031519f69d
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-25 21:32:26 +0530

    Update Exception classes

--- src/main/java/com/capstone/wfh/exception/WeekendException.java
+++ src/main/java/com/capstone/wfh/exception/WeekendException.java
@@ -1,7 +1,7 @@
 package com.capstone.wfh.exception;
 
 public class WeekendException extends RuntimeException {
-    public WeekendException(String message) {
-        super(message);
+    public WeekendException() {
+        super("ERROR: Provided from-date and to-date must not be a weekend.");
     }
 }
