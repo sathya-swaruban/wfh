3ec66669f853ca46fd69cc97faf878f2b020a02d
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-27 21:42:01 +0530

    Update WfhRepository

--- src/main/java/com/capstone/wfh/repository/WfhRepository.java
+++ src/main/java/com/capstone/wfh/repository/WfhRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface WfhRepository extends MongoRepository<WfhRequest, String> {
+
     List<WfhRequest> findByFromDate(String fromDate);
+
 }
