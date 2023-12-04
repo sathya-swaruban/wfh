bae82251d58c071280836cc8dba04d971f02a4d3
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-12-04 20:49:55 +0530

    Update WfhApplicationTests

--- src/test/java/com/capstone/wfh/WfhApplicationTests.java
+++ src/test/java/com/capstone/wfh/WfhApplicationTests.java
@@ -1,13 +1,42 @@
 package com.capstone.wfh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WfhApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnAWfhRequestWhenDataIsSaved() throws URISyntaxException {
+        URI uri = new URI("http://localhost:" + port + "/submit");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String json = """
+                {
+                    "fromDate": "2023-12-01",
+                    "toDate": "2023-12-01",
+                    "fromTime": "08:00",
+                    "toTime": "17:15"
+                }
+                """;
+        HttpEntity<String> entity = new HttpEntity<>(json, headers);
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
 
 }
