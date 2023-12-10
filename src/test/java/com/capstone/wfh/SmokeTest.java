acf79ec9a38488fb9cd2f89d79eb14e38428b734
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-12-10 19:44:19 +0530

    Upload SmokeTest

--- src/test/java/com/capstone/wfh/SmokeTest.java
+++ src/test/java/com/capstone/wfh/SmokeTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@SuppressWarnings(value = "all")
 class SmokeTest {
 
     @Autowired
