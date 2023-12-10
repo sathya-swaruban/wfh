008bd20acbbb233b58d88aa60209913ea7027a6e
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-12-10 19:44:30 +0530

    Upload WfhRequestJSONTest

--- src/test/java/com/capstone/wfh/WfhRequestJSONTest.java
+++ src/test/java/com/capstone/wfh/WfhRequestJSONTest.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
+@SuppressWarnings(value = "all")
 public class WfhRequestJSONTest {
 
     @Autowired
